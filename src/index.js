e62b8ff31adb861be588dba960c98e0ad21dbd01
sqmeonee <ewefwef31@gmail.com>
2023-09-24 11:15:09 -0400

    Added a way to disable buttons

--- src/index.js
+++ src/index.js
@@ -14,16 +14,12 @@ async function slashPaginate(options = {}) {
         if(!options.emojis.none) options.emojis.none = "⏹️";
         if(!options.emojis.right) options.emojis.right = "▶️";
         if(!options.emojis.end) options.emojis.end = "⏩";
-
-        if (!options.disable.beginning) options.disable.beginning = false;
-        if (!options.disable.end) options.disable.end = false;
-        if (!options.disable.placeholder) options.disable.placeholder = false;
-        
-        if (typeof options.disable.beginning !== 'boolean') throw new TypeError("disable.beginning is not of type boolean")
-        if (typeof options.disable.end !== 'boolean') throw new TypeError("disable.end is not of type boolean")
-        if (typeof options.disable.placeholder !== 'boolean') throw new TypeError("disable.placeholder is not of type boolean")
     
-        if(!Array.isArray(options.pages)) throw new TypeError('Pages is not of type array')
+        if(!Array.isArray(options.pages)) throw new TypeError('Pages must be an array')
+
+        if (!options.disable) options.disable = {};
+        if (!options.disable.first_last) options.disable.first_last = false;
+        if (!options.disable.placeholder) options.disable.placeholder = false;
     
         if(!options.buttonstyles) options.buttonstyles = {};
         if(!options.buttonstyles.beginning) options.buttonstyles.beginning = "Primary";
@@ -106,14 +102,19 @@ async function slashPaginate(options = {}) {
         .setEmoji(options.emojis.end)
         .setStyle(options.buttonstyles.end)
     
-        const buttonRow = new ActionRowBuilder() // setComponents, addComponents
-
-        if (options.disable.beginning == false) buttonRow.addComponents(beginning)
-        buttonRow.addComponents(prev)
-        if (options.disable.placeholder == false) buttonRow.addComponents(none)
-        buttonRow.addComponents(next)
-        if (options.disable.end == false) buttonRow.addComponents(end) 
-
+        let buttonRow = new ActionRowBuilder().setComponents(beginning, prev, none, next, end)
+        if (options.disable.placeholder == true) {
+            if (options.disable.first_last == true) {
+                buttonRow = buttonRow.setComponents(prev, next)
+            }
+            else buttonRow = buttonRow.setComponents(beginning, prev, next, end)
+        }
+        if (options.disable.first_last == true) {
+            if (options.disable.placeholder == true) {
+                buttonRow = buttonRow.setComponents(prev, next)
+            }
+            else buttonRow = buttonRow.setComponents(prev, none, next)
+        }
         let index = 0;
     
         if (typeof options.pages[0] == 'object') {
@@ -240,7 +241,171 @@ async function slashPaginate(options = {}) {
 }
 
 
+//prefix
+async function prefixPaginate(options) {
+    if(!options.message) throw new TypeError('Provide a message argument')
+    if(!options.pages) throw new TypeError('Provide a page argument')
+    if(!options.emojis) options.emojis = {};
+    
+    if(!options.emojis.beginning) options.emojis.beginning = "⏪";
+    if(!options.emojis.left) options.emojis.left = "◀️";
+    if(!options.emojis.none) options.emojis.none = "⏹️";
+    if(!options.emojis.right) options.emojis.right = "▶️";
+    if(!options.emojis.end) options.emojis.end = "⏩";
+
+    if(!Array.isArray(options.pages)) throw new TypeError('Pages must be an array')
+
+    if (!options.disable) options.disable = {};
+    if (!options.disable.first_last) options.disable.first_last = false;
+    if (!options.disable.placeholder) options.disable.placeholder = false;
+
+    if(!options.buttonstyles) options.buttonstyles = {};
+    if(!options.buttonstyles.beginning) options.buttonstyles.beginning = "Primary";
+    if(!options.buttonstyles.left) options.buttonstyles.left = "Primary";
+    if(!options.buttonstyles.none) options.buttonstyles.none = "Secondary";
+    if(!options.buttonstyles.right) options.buttonstyles.right = "Primary";
+    if(!options.buttonstyles.end) options.buttonstyles.end = "Primary";
+
+    if(typeof options.emojis.beginning !== 'string') throw new TypeError('Emojis need to be in a string format');
+    if(typeof options.emojis.left !== 'string') throw new TypeError('Emojis need to be in a string format');
+    if(typeof options.emojis.none !== 'string') throw new TypeError('Emojis need to be in a string format');
+    if(typeof options.emojis.right !== 'string') throw new TypeError('Emojis need to be in a string format');
+    if(typeof options.emojis.end !== 'string') throw new TypeError('Emojis need to be in a string format');
+
+    if (typeof options.disable.first_last !== "boolean") throw new TypeError("Disabling first and last buttons is not of type boolean")
+    if (typeof options.disable.placeholder !== "boolean") throw new TypeError("Disabling placeholder button is not of type boolean")
+
+    if(options.buttonstyles.left === 'Primary') options.buttonstyles.beginning = ButtonStyle.Primary
+        else if(options.buttonstyles.left === 'Secondary') options.buttonstyles.beginning = ButtonStyle.Secondary
+        else if(options.buttonstyles.left === 'Danger') options.buttonstyles.beginning = ButtonStyle.Danger
+        else if(options.buttonstyles.left === 'Success') options.buttonstyles.beginning = ButtonStyle.Success
+        else if(options.buttonstyles.left === 'Link') throw new TypeError('Button style cannot be "Link"')
+        else throw new TypeError('Give a valid ButtonStyle')
+
+    if(options.buttonstyles.none === 'Primary') options.buttonstyles.none = ButtonStyle.Primary
+        else if(options.buttonstyles.none === 'Secondary') options.buttonstyles.beginning = ButtonStyle.Secondary
+        else if(options.buttonstyles.none === 'Danger') options.buttonstyles.beginning = ButtonStyle.Danger
+        else if(options.buttonstyles.none === 'Success') options.buttonstyles.beginning = ButtonStyle.Success
+        else if(options.buttonstyles.none === 'Link') throw new TypeError('Button style cannot be "Link"')
+        else throw new TypeError('Give a valid ButtonStyle')
+
+    if(options.buttonstyles.right === 'Primary') options.buttonstyles.beginning = ButtonStyle.Primary
+        else if(options.buttonstyles.right === 'Secondary') options.buttonstyles.beginning = ButtonStyle.Secondary
+        else if(options.buttonstyles.right === 'Danger') options.buttonstyles.beginning = ButtonStyle.Danger
+        else if(options.buttonstyles.right === 'Success') options.buttonstyles.beginning = ButtonStyle.Success
+        else if(options.buttonstyles.right === 'Link') throw new TypeError('Button style cannot be "Link"')
+        else throw new TypeError('Give a valid ButtonStyle')
+
+    if(options.buttonstyles.end === 'Primary') options.buttonstyles.beginning = ButtonStyle.Primary
+        else if(options.buttonstyles.end === 'Secondary') options.buttonstyles.beginning = ButtonStyle.Secondary
+        else if(options.buttonstyles.end === 'Danger') options.buttonstyles.beginning = ButtonStyle.Danger
+        else if(options.buttonstyles.end === 'Success') options.buttonstyles.beginning = ButtonStyle.Success
+        else if(options.buttonstyles.end === 'Link') throw new TypeError('Button style cannot be "Link"')
+        else throw new TypeError('Give a valid ButtonStyle')
+
+    if (options.pages.length === 1) {
+       const page = await options.message.reply({
+        embeds: options.pages,
+        components: [],
+        fetchReply: true
+       })
+
+       return page
+    }
+    const beginning = new ButtonBuilder()
+    .setCustomId('beginning')
+    .setEmoji(options.emojis.beginning)
+    .setStyle(options.buttonstyles.beginning)
+    .setDisabled(true)
+
+    const prev = new ButtonBuilder()
+    .setCustomId('prev')
+    .setEmoji(options.emojis.left)
+    .setStyle(options.buttonstyles.left)
+    .setDisabled(true)
+
+    const none = new ButtonBuilder()
+    .setCustomId('none')
+    .setEmoji('⏹️')
+    .setStyle(options.buttonstyles.none)
+    .setDisabled(true)
+
+    const next = new ButtonBuilder()
+    .setCustomId('next')
+    .setEmoji(options.emojis.right)
+    .setStyle(options.buttonstyles.right)
+
+    const end = new ButtonBuilder()
+    .setCustomId('end')
+    .setEmoji(options.emojis.end)
+    .setStyle(options.buttonstyles.end)
+
+    let buttonRow = new ActionRowBuilder().addComponents(beginning, prev, none, next, end)
+    if (options.disable.first_last == true) {
+        if (options.disable.placeholder == true) {
+            buttonRow = buttonRow.setComponents(prev, next)
+        } else buttonRow = buttonRow.setComponents(prev, none, next)
+    } else if (options.disable.placeholder == true) {
+        buttonRow = buttonRow.setComponents(beginning, prev, next, end)
+    }
+    let index = 0;
+
+    const currentPage = await options.message.reply({
+        embeds: [options.pages[index]],
+        components: [buttonRow],
+        fetchReply: true
+    })
+
+    const collector = await currentPage.createMessageComponentCollector({
+        componentType: ComponentType.Button,
+    })
+
+    collector.on('collect', async (i) => {
+        if (i.user.id !== options.message.author.id) return await i.reply({ content: 'You can\'t use these buttons', ephemeral: true})
+
+        await i.deferUpdate()
+
+        if(i.customId == 'prev') {
+            if(index > 0) index--;
+        } else if(i.customId == 'next') {
+            if (index < options.pages.length - 1) index++;
+        } else if(i.customId == 'beginning') {
+            index = 0;
+        } else if(i.customId == 'end') {
+            index = options.pages.length - 1;
+        }
+
+        if (index == 0) prev.setDisabled(true)
+        else prev.setDisabled(false)
+
+        if (index == 0) beginning.setDisabled(true)
+        else beginning.setDisabled(false)
+
+        if (index == options.pages.length - 1) next.setDisabled(true)
+        else next.setDisabled(false)
+
+        if (index == options.pages.length - 1) end.setDisabled(true)
+        else end.setDisabled(false)
+
+        await currentPage.edit({
+            embeds: [options.pages[index]],
+            components: [buttonRow],
+        })
+
+    })
+
+    collector.on('end', async (i) => {
+        prev.setDisabled(true)
+        none.setDisabled(true)
+        next.setDisabled(true)
+        await currentPage.edit({
+            embeds: [options.pages[index]],
+            components: [buttonRow]
+        })
+    })
+}
 
 module.exports = { 
-    slashPaginate
+    slashPaginate, 
+    prefixPaginate
 }
